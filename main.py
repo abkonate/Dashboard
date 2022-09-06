f6ff4c797936816f35612506c04665465e3e29c0
abkonate <papisco3@hotmail.com>
2022-09-06 17:36:20 +0200

    commit

--- main.py
+++ main.py
@@ -15,22 +15,45 @@ from bokeh.models import PreText
 from bokeh.models.widgets import TextInput
 import pandas_bokeh
 import os
+import gdown
+from bokeh.models import CustomJS, ColumnDataSource, Select, Column
+from bokeh.plotting import figure, show
+from bokeh.models.widgets import Div
+from bokeh.models.widgets import Paragraph
+from bokeh.models.widgets import PreText
+from bokeh.layouts import row
+from bokeh.models.widgets import TextInput
+import pandas_bokeh
+from bokeh.io import curdoc
 
 
+if os.path.isfile('./Projet+Mise+en+prod+-+home-credit-default-risk.zip') == False:
+  output="Projet+Mise+en+prod+-+home-credit-default-risk.zip"
+  url = "https://drive.google.com/file/d/1WMSzXPeZzgZB9WvbEg9KT-nZom_jdhMN/view?usp=sharing"
+  gdown.download(url=url, output=output, quiet=False, fuzzy=True)
 
-data_prediction = pd.read_csv(join(dirname(__file__), 'application_proba_prediction.csv'))
+if os.path.isfile('./application_proba_prediction.csv') == False:
+  output2 ="application_proba_prediction.csv" 
+  url2 = "https://drive.google.com/file/d/1-14BNXfPwiEP5pN1ornSj0Ct-jy2T19z/view?usp=sharing"
+  gdown.download(url=url2, output=output2, quiet=False, fuzzy=True)
 
-absolute_path = os.path.abspath(os.path.dirname('HomeCredit_columns_description.csv'))
+import shutil
 
-HomeCredit_columns_description = pd.read_csv(absolute_path + '/HomeCredit_columns_description.csv' ,encoding='cp1252')
+if os.path.isfile('./Projet+Mise+en+prod+-+home-credit-default-risk.zip') == False:
+  shutil.unpack_archive('Projet+Mise+en+prod+-+home-credit-default-risk.zip', './')
 
+data_prediction = pd.read_csv("./application_proba_prediction.csv")
+data_prediction = data_prediction.drop('Unnamed: 0', axis=1)
+
+HomeCredit_columns_description = pd.read_csv('./HomeCredit_columns_description.csv',encoding='cp1252')
+HomeCredit_columns_description = HomeCredit_columns_description.drop('Unnamed: 0', axis=1)
 
 Var_Name = HomeCredit_columns_description[['Row','Description']]
-
 Var_Name = Var_Name.drop_duplicates(subset=['Row'])
 
 
-df_train = pd.read_csv(join(dirname(__file__),"application_train.csv"))
+df_train = pd.read_csv("./application_train.csv")
+df_test  = pd.read_csv("./application_test.csv")
 
 df = data_prediction.copy()
 
@@ -52,70 +75,124 @@ expl1 = Div(text=""" La probabilité sur risque de rembpursement est calculée s
 
 
 
-select2 = Select(title="Choisir une variable:", value='AMT_CREDIT' , options=list(Var_Name['Row'].values))
+select2 = Select(title="Choisir une variable:", value='AMT_REQ_CREDIT_BUREAU_YEAR' , options=list(Var_Name['Row'].values))
 
-df_desc = Var_Name[Var_Name['Row'] == 'NAME_CONTRACT_TYPE' ]
-
-#df_train_var = df_train[['TARGET', select2.value]]
-
-#df_train_var = df_train_var.dropna()
-
-
-#p_scatter = df_train_var.plot_bokeh.scatter(
-#    figsize=(900, 600),
-#    x="TARGET",
-#    y= select2.value,
-#    category="TARGET",
-#    title="Variable choisie en fonction target",
-#    #size="CNT_CHILDREN",
-#    show_figure=False
-#    )
-
-
-description_of_variable = TextInput(value = str(df_desc['Description'].values) , title = "Description de la varibale choisie :")
-
-
-Title2 = Div(text=""" <h1> La probabilité du risque de remboursement est estimée à  </h1>""" , width=800, height=100)
-
-
-proba_remb = TextInput(value = str(0.5) , title = "")
-
-df_aux = df [df['SK_ID_CURR'] == int(select.value)]
-
-proba_remb.value = str(df_aux['label'].values)
-
+df_desc = Var_Name[Var_Name['Row'] == 'AMT_REQ_CREDIT_BUREAU_YEAR' ]
 
 
 def create_figure():
   df_train_var = df_train[['TARGET', select2.value]]
   df_train_var = df_train_var.dropna()
-  xs = df_train_var['TARGET'].values
-  ys = df_train_var[select2.value].values
 
-  x_title = 'Target'
+  df_train_var_1 = df_train_var[ df_train_var['TARGET'] == 1 ]
+  df_train_var_0 = df_train_var[ df_train_var['TARGET'] == 0 ]
+
+  xs = df_train_var_1['TARGET'].values
+  ys = df_train_var_1[select2.value].values
+
+  x_title = 'Distribution'
   y_title = select2.value
 
   dic_cat = {}
 
+  
   if df_train_var[select2.value].dtype == object:
-    kw = list(set(ys))
+    kw = list(set(df_train_var[select2.value].values))
     nb_cat  = len(kw)
     for i in range(nb_cat):
       dic_cat[kw[i]] = i
     
     ys =[ dic_cat[a] for a in list(ys) ]
 
-  p = figure(height=500, width=800, tools='pan,box_zoom,hover,reset')
+    text_des = ""
+
+    for keys,values in dic_cat.items():
+      text_des +=  str(keys) +" " + str(values)+ " "
+
+    print(text_des)
+
+    x_title = text_des
+    
+
+  if df_train_var_1[select2.value].dtype == object:
+    bins  = np.array(list(set(ys)))
+  elif df_train_var_1[select2.value].dtype == np.int64:
+    bins  = np.array(list(set(ys)))
+  else:
+    bins = np.linspace(ys.min(), ys.max(), 100)
+
+  hist, edges = np.histogram(ys, density=True, bins=bins)
+
+  #print(edges)
+
+  p = figure(height=600, width=800, tools='pan,box_zoom,hover,reset')
   p.xaxis.axis_label = x_title
   p.yaxis.axis_label = y_title
 
-  p.circle(x=xs, y=ys, line_color="white", alpha=0.6, hover_color='white', hover_alpha=0.5, size=10)
+  p.quad(top=hist, bottom=0, left=edges[:-1], right=edges[1:],
+         fill_color="skyblue", line_color="white",
+         legend_label=f"label 1")
+  
+  # Probability density function
+  #x = np.linspace(-5.0, 5.0, len(ys))
+  #p.line(x, ys, line_width=2, line_color="navy",
+  #legend_label="Probability Density Function")
+
+  #p.circle(x=xs, y=ys, line_color="white", alpha=0.6, hover_color='white', hover_alpha=0.5, size=10)
+
+  xs = df_train_var_0['TARGET'].values
+  ys = df_train_var_0[select2.value].values
+
+  #x_title = 'Target'
+  #y_title = select2.value
+
+  dic_cat = {}
+
+  if df_train_var[select2.value].dtype == object:
+    kw = list(set(df_train_var[select2.value].values))
+    nb_cat  = len(kw)
+    for i in range(nb_cat):
+      dic_cat[kw[i]] = i
+    
+    ys =[ dic_cat[a] for a in list(ys) ]
+    #print(dic_cat)
+
+  #print(ys.min())
+  #print(ys.max())
+  #print(len(ys))
+  #bins = np.linspace(ys.min(), ys.max(), 10)
+
+  #print(bins)
+
+  hist, edges = np.histogram(ys, density=True, bins=bins)
+
+  #print(edges)
+
+  #p = figure(height=600, width=800, tools='pan,box_zoom,hover,reset')
+  #p.xaxis.axis_label = x_title
+  #p.yaxis.axis_label = y_title
+
+  p.quad(top=hist, bottom=0, left=edges[:-1], right=edges[1:],
+         fill_color="skyblue", line_color="red",
+         legend_label=f"label 0")
 
   return p
 
 
 def update(attr, old, new):
-    Col2.children[2] = create_figure()
+    Col2.children[1] = create_figure()
+
+
+description_of_variable = TextInput(value = str(df_desc['Description'].values) , title = "Description de la varibale choisie :")
+
+df_aux = df_test [df_test['SK_ID_CURR'] == int(select.value)]
+
+info_of_customer = Div(text=(df_aux.T).to_html(index=True), width=550)
+#info_of_customer = TextInput(value = str(df_aux) , title = "information client :")
+
+Title2 = Div(text=""" <h1> La probabilité du risque de remboursement est estimée à  </h1>""" , width=800, height=100)
+
+proba_remb = TextInput(value = str(0.5) , title = "")
 
 def variable_choisie(attrname, old, new):
   p_scatter = df_train_var.plot_bokeh.scatter(
@@ -130,105 +207,35 @@ def variable_choisie(attrname, old, new):
   
 def client_choisi(attrname, old, new):
   df_aux = df [df['SK_ID_CURR'] == int(select.value)]
-  proba_remb.value = str(df_aux['label'].values)
-  #print(df_aux['label'].values)
+  print(df_aux['label'].values)
+  df_aux = df_test [df_test['SK_ID_CURR'] == int(select.value)]
+  Col3.children[0] = Div(text=(df_aux.T).to_html(index=True), width=550)
 
 
 
 select.on_change('value', client_choisi)
-
-#select2.on_change('value', variable_choisie)
 
 select2.on_change('value', update)
 
 
 Col1 = Column(Title, select, select2, expl1, description_of_variable)
 
-#Col2 = Column(Title2, proba_remb, p_scatter)
-
 Col2 = Column(Title2, proba_remb, create_figure())
 
-# add to document
-#curdoc().add_root(row(Col1,Col2))
-#curdoc().title = "Clustering"
+Col3 = Column(info_of_customer)
 
-#show(row(Col1,Col2))
-
-
-
-
-#data_prediction = pd.read_csv('application_test_prediction.csv')
-
-#df = data_prediction.copy()
-
-#df_index = list(map(str, df['SK_ID_CURR']))
-
-
-
-
-
-#select = Select(title="Custoimer ID:", value='106861',
-#                options=df_index)
-
-
-
-#div = Div(text=""" Le risque de remboursement du client """ + select.value +""" est de""", width=200, height=100)
-
-
-#LABELS = ["Afficher la probabilité du risque de remboursement", 
-#          "Afficher les informations du client", 
-#          "Afficher les indicateurs de comparaisons avec les autres clients"]
-
-#checkbox_group = CheckboxGroup(labels=LABELS, active=[1])
-
-
-#df_aux = df [df['SK_ID_CURR'] == int('106861')]
-
-#description = PreText(text="la probabilité du risque de rembouresement est estimée à" + str(df_aux['label'].values), width=500)
-
-#div = Div(text=""" ii""", width=200, height=100)
-#for col in df_aux.columns:
-#  div.text = div.text + col +"      " + str(df_aux[col].values) + "     "  + '</br>'
-
-
-
-
-#div = Div(text=str(df_aux.to_html()), width=200, height=100)
-
-
-
-#def client_choisi(attrname, old, new):
-#  df_aux = df [df['SK_ID_CURR'] == int(select.value)]
-#  description.text = str(df_aux['label'])
-#  div = Div(text=""" Le risque de remboursement du client """ + select.value +""" est de""", width=200, height=1000)
-#  description_of_variable.value = "apres hanget"
-  
-
-#def information_client(attrname, old, new):
-#  df_aux = df [df['SK_ID_CURR'] == int('106861')]
-#  if 0 in checkbox_group.active:
-#    div = Div(text="""""", width=200, height=100)
-#    for col in df_aux.columns:
-#      div.text = div.text + col +"      " + str(df_aux[col].values) + "     "  + '</br>'
-#  if 1 in checkbox_group.active:
-#    print('fonction à définir')
-#  if 2 in checkbox_group.active:
-#    print('fonction à définir')
-
-
-#description_of_variable = TextInput(value = "Debut" , title = "Description :")
-
-
-#select.on_change('value', client_choisi)
-
-#div.on_change('text', client_choisi)
-
-#checkbox_group.on_change('labels', information_client)
-
-#controle = Column(select, checkbox_group, div,description_of_variable)
-
-#show(row(controle, div))
+layout = row(Col1,Col2, Col3) 
 
 # add to document
-curdoc().add_root(row(Col1,Col2))
-curdoc().title = "Dash"
+curdoc().add_root(layout)
+curdoc().title = "Dashboard"
+
+#layout = row(Col1,Col2, Col3) 
+
+#show(layout)
+
+
+
+
+
+
